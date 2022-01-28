392c35e516d6e02d55ae8464856570018da9b62b
FiniteReality <digpoealternate@gmail.com>
2022-01-28 20:45:56 +0000

    Remove chunk name string to try fixing gcc

--- runtime/story/loading.cpp
+++ runtime/story/loading.cpp
@@ -37,7 +37,7 @@ story fabulist::load_story_from_stream(std::istream& stream)
     story story;
 
     reader tmpReader{stream, std::vector<char>(4096)};
-    auto code = story._impl->L.script(read_func, &tmpReader, "story");
+    auto code = story._impl->L.script(read_func, &tmpReader);
 
     return story;
 }
