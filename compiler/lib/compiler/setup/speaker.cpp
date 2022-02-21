84474fb4d30f4596c175531da145994180bd44ba
FiniteReality <digpoealternate@gmail.com>
2022-02-21 00:29:16 +0000

    Add speaker lines to the current section

--- compiler/lib/compiler/setup/speaker.cpp
+++ compiler/lib/compiler/setup/speaker.cpp
@@ -1,4 +1,10 @@
+#include <string_view>
+
 #include "common.hpp"
+
+constexpr std::pair<std::string_view, std::string_view> values[] = {
+    {"type", "line"},
+};
 
 static int speaker_line(lua_State* L)
 {
@@ -12,7 +18,33 @@ static int speaker_line(lua_State* L)
     (void)line;
     (void)speaker;
 
+    lua_createtable(L, 0, 1 + (int)std::size(values));
+
+    lua_pushliteral(L, "text");
+    lua_pushvalue(L, 1);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "speaker");
+    lua_pushvalue(L, lua_upvalueindex(1));
+    lua_settable(L, -3);
+
+    for (auto& pair : values)
+    {
+        lua_pushlstring(L, pair.first.data(), pair.first.size());
+        lua_pushlstring(L, pair.second.data(), pair.second.size());
+        lua_settable(L, -3);
+    }
+
     lua_pushvalue(L, LUA_REGISTRYINDEX); // +registry
+
+    lua_pushliteral(L, "current_section"); // k
+    lua_rawget(L, -2); // registry[k]
+    size_t size = lua_rawlen(L, -1); // #registry[k]
+    lua_pushvalue(L, -3); // v
+    lua_rawseti(L, -2, size + 1); // registry[k][#registry[k] + 1] = v
+
+    lua_pop(L, 1);
+
     lua_pushliteral(L, "speakers"); // k
     lua_rawget(L, -2); // registry[k]
     lua_pushvalue(L, lua_upvalueindex(1)); // k2
